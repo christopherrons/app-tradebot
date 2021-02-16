5b532dc39c12a83472ea656051b9a7ec163a3bd8
Thomas <brunnner>
2021-02-16 19:36:28 +0100

    API

---
+++ Implementation/utils.py
@@ -0,0 +1,67 @@
+import requests
+import hmac
+import hashlib
+import time
+
+
+_API_URL = 'https://www.bitstamp.net/api/'
+
+
+class APIError(Exception):
+    pass
+
+
+class APIMixin(object):
+    url = None
+    method = 'get'
+
+    def _process_response(self, response):
+        """
+        Allows customized response to be returned.
+        """
+        return
+
+    def call(self, **params):
+        # Form request
+        r = None
+        url = _API_URL + self.url
+        if self.method == 'get':
+            r = requests.get(url, params=params)
+        elif self.method == 'post':
+            r = requests.post(url, data=params)
+        response = r.json()
+        if isinstance(response, dict) and 'error' in response:
+            raise APIError(response['error'])
+        new_response = self._process_response(response)
+        if new_response is not None:
+            response = new_response
+        return response
+
+
+class APIAuthMixin(APIMixin):
+    method = 'post'
+
+    def __init__(self, customer_id, api_key, api_secret):
+        self.customer_id = customer_id
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def get_nonce(self):
+        return bytes(str(int(time.time() * 1e6)), 'utf-8')
+
+    def call(self, **params):
+        nonce = self.get_nonce()
+        message = nonce + self.customer_id + self.api_key
+        signature = hmac.new(
+            self.api_secret,
+            msg=message,
+            digestmod=hashlib.sha256).hexdigest().upper()
+        params.update({
+            'key': self.api_key, 'signature': signature, 'nonce': nonce
+        })
+        return super(APIAuthMixin, self).call(**params)
+
+
+class APIOpenOrdersCall(APIAuthMixin):
+    url = 'open_orders/'
+
